18f567671fd173fe17a0279796606cb518d59903
nabeelmukhtar <nabeelmukhtar@951eb183-79ad-187b-355e-4f29082537e9>
2010-08-27 10:38:22 +0000

    Fixed compilation errors.

--- trunk/uclassify-java-sdk/core/src/main/java/com/google/code/uclassify/client/impl/AsyncUClassifyClientAdapter.java
+++ trunk/uclassify-java-sdk/core/src/main/java/com/google/code/uclassify/client/impl/AsyncUClassifyClientAdapter.java
@@ -3,14 +3,18 @@
  */
 package com.google.code.uclassify.client.impl;
 
+import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
-import com.google.code.uclassify.client.AsyncResponseHandler;
 import com.google.code.uclassify.client.AsyncUClassifyClient;
 import com.google.code.uclassify.client.UClassifyClient;
 import com.google.code.uclassify.client.UClassifyConsumer;
-import com.uclassify.api._1.responseschema.ResponseEntity;
+import com.uclassify.api._1.responseschema.ClassInformation;
+import com.uclassify.api._1.responseschema.Classification;
 
 /**
  * @author Nabeel Mukhtar
@@ -19,7 +23,10 @@ import com.uclassify.api._1.responseschema.ResponseEntity;
 public class AsyncUClassifyClientAdapter implements AsyncUClassifyClient {
 
     /** Field description */
-    private AsyncHandlerUClassifyClientAdapter client;
+    private UClassifyClient client;
+    
+    /** Field description */
+    private ExecutorService taskExecutor;
     
     /** Field description */
 
@@ -30,7 +37,8 @@ public class AsyncUClassifyClientAdapter implements AsyncUClassifyClient {
      * @param client
      */
     public AsyncUClassifyClientAdapter(UClassifyClient client, ExecutorService taskExecutor) {
-        this.client  = new AsyncHandlerUClassifyClientAdapter(client, taskExecutor);
+        this.client  = client;
+        this.taskExecutor = taskExecutor;
     }
 
     /**
@@ -49,6 +57,32 @@ public class AsyncUClassifyClientAdapter implements AsyncUClassifyClient {
     	client.setApiConsumer(apiConsumer);
     }
     
+    /**
+     * Method description
+     *
+     *
+     * @param task
+     *
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+	protected Future execute(Runnable task) {
+        return taskExecutor.submit(task);
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param task
+     * @param <T>
+     *
+     * @return
+     */
+    protected <T> Future<T> execute(Callable<T> task) {
+        return taskExecutor.submit(task);
+    }
+
     /**
      * Sets the request headers.
      *
@@ -86,10 +120,139 @@ public class AsyncUClassifyClientAdapter implements AsyncUClassifyClient {
         client.removeRequestHeader(headerName);
     }
     
-    private static class NullResponseHandler<T extends ResponseEntity> extends AsyncResponseHandler<T> {
-		@Override
-		public void handleResponse(T response) {
-			// No-Op
-		} 
-    }
+	@Override
+	public Future<?> addClass(final String classifierName, final String className) {
+		return execute(new Runnable() {
+            @Override
+            public void run() {
+                client.addClass(classifierName, className);
+            }
+        });
+	}
+
+	@Override
+	public Future<Map<String, Classification>> classify(final String classifierName,
+			final List<String> texts) {
+		return execute(new Callable<Map<String, Classification>>() {
+            @Override
+            public Map<String, Classification> call() throws Exception {
+                return client.classify(classifierName, texts);
+            }
+        });
+	}
+
+	@Override
+	public Future<Map<String, Classification>> classify(final String classifierName,
+			final InputStream texts) {
+		return execute(new Callable<Map<String, Classification>>() {
+            @Override
+            public Map<String, Classification> call() throws Exception {
+                return client.classify(classifierName, texts);
+            }
+        });
+	}
+
+	@Override
+	public Future<Map<String, Classification>> classify(final String userName,
+			final String classifierName, final List<String> texts) {
+		return execute(new Callable<Map<String, Classification>>() {
+            @Override
+            public Map<String, Classification> call() throws Exception {
+                return client.classify(userName, classifierName, texts);
+            }
+        });
+	}
+
+	@Override
+	public Future<Map<String, Classification>> classify(final String userName,
+			final String classifierName, final InputStream texts) {
+		return execute(new Callable<Map<String, Classification>>() {
+            @Override
+            public Map<String, Classification> call() throws Exception {
+                return client.classify(userName, classifierName, texts);
+            }
+        });
+	}
+
+	@Override
+	public Future<?> createClassifier(final String classifierName) {
+		return execute(new Runnable() {
+            @Override
+            public void run() {
+                client.createClassifier(classifierName);
+            }
+        });
+	}
+
+	@Override
+	public Future<List<ClassInformation>> getInformation(final String classifierName) {
+		return execute(new Callable<List<ClassInformation>>() {
+            @Override
+            public List<ClassInformation> call() throws Exception {
+                return client.getInformation(classifierName);
+            }
+        });
+	}
+
+	@Override
+	public Future<?> removeClass(final String classifierName, final String className) {
+		return execute(new Runnable() {
+            @Override
+            public void run() {
+                client.removeClass(classifierName, className);
+            }
+        });
+	}
+
+	@Override
+	public Future<?> removeClassifier(final String classifierName) {
+		return execute(new Runnable() {
+            @Override
+            public void run() {
+                client.removeClassifier(classifierName);
+            }
+        });
+	}
+
+	@Override
+	public Future<?> train(final String classifierName,
+			final Map<String, String> trainingTexts) {
+		return execute(new Runnable() {
+            @Override
+            public void run() {
+                client.train(classifierName, trainingTexts);
+            }
+        });
+	}
+
+	@Override
+	public Future<?> train(final String classifierName, final InputStream trainingTexts) {
+		return execute(new Runnable() {
+            @Override
+            public void run() {
+                client.train(classifierName, trainingTexts);
+            }
+        });
+	}
+
+	@Override
+	public Future<?> untrain(final String classifierName,
+			final Map<String, String> trainingTexts) {
+		return execute(new Runnable() {
+            @Override
+            public void run() {
+                client.untrain(classifierName, trainingTexts);
+            }
+        });
+	}
+
+	@Override
+	public Future<?> untrain(final String classifierName, final InputStream trainingTexts) {
+		return execute(new Runnable() {
+            @Override
+            public void run() {
+                client.untrain(classifierName, trainingTexts);
+            }
+        });
+	}
 }
