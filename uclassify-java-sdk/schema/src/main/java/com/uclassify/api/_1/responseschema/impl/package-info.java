20f0bf246b61c59082755b57fe8f943413814bf0
nabeelmukhtar <nabeelmukhtar@951eb183-79ad-187b-355e-4f29082537e9>
2010-08-27 09:50:33 +0000

    Fixed compilation errors.

---
+++ uclassify-java-sdk/schema/src/main/java/com/uclassify/api/_1/responseschema/impl/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://api.uclassify.com/1/ResponseSchema", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.uclassify.api._1.responseschema.impl;
