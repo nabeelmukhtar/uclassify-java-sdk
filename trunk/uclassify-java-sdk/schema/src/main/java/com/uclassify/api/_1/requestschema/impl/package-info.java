c0bb6b8cac715401c7244a406cefcd5b42e9e5c8
nabeelmukhtar <nabeelmukhtar@951eb183-79ad-187b-355e-4f29082537e9>
2010-08-27 09:50:33 +0000

    Fixed compilation errors.

---
+++ trunk/uclassify-java-sdk/schema/src/main/java/com/uclassify/api/_1/requestschema/impl/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://api.uclassify.com/1/RequestSchema", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.uclassify.api._1.requestschema.impl;
